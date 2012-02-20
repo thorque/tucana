f493ed3144caedad402e5dd0e72cef418344b25b
Thorsten Kamann <thorsten.kamann@googlemail.com>
2012-02-20 21:52:09 +0100

    #5: Create an importer component to get data from wikipedia https://bitbucket.org/thorque/tucana/issue/5/

--- tucana-integration/src/test/groovy/org/tucana/service/ConstellationServiceITTest.groovy
+++ tucana-integration/src/test/groovy/org/tucana/service/ConstellationServiceITTest.groovy
@@ -7,13 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 
-/**
- * Created by IntelliJ IDEA.
- * User: kamann
- * Date: 13.02.12
- * Time: 21:40
- * To change this template use File | Settings | File Templates.
- */
+
 @RunWith(SpringJUnit4ClassRunner)
 @ContextConfiguration
 class ConstellationServiceITTest {
@@ -23,6 +17,6 @@ class ConstellationServiceITTest {
 
     @Test
     public final void "test the findAllConstellation service method"() {
-        Assert.assertEquals(2, service.findAllConstellations().size())
+        Assert.assertEquals(88, service.findAllConstellations().size())
     }
 }
--- tucana-integration/src/test/resources/database/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO CONSTELLATIONS
-        VALUES (null, 'Andromeda', 'AND', 'Andromedae', 'N', 'Ptolemaeus', 1456, 34.5);
-INSERT INTO CONSTELLATIONS
-        VALUES (null, 'Orion', 'ORI', 'Orionis', 'N', 'Ptolemaeus', 1456, 20.5);
